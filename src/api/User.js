7f0a754da4f959791b6b02a538d7d9260b5f3595
tuan huy <100185063+huynopro102@users.noreply.github.com>
2024-07-12 15:36:38 +0700

    "change user to User"

--- src/api/User.js
+++ src/api/User.js
@@ -1,20 +1,67 @@
 import axios from "../../axios";
 
-const getAll = () =>{
-    return axios.get("/api/user/getListUsers")
-}
+
+const demo = (idUser, data) => {
+  return axios.post(`/api/user/editUser/${idUser}`, data);
+};
+
+const updateUser = (idUser, data) => {
+  return axios.post(`/api/user/editUser/${idUser}`, data);
+};
+const getAllUsers = () => {
+    return axios.get("/api/user/getListUsers");
+};
+
+const getListUsersNotBanned = () => {
+  return axios.get("/api/user/getListUsersNotBanned");
+};
+
+const bannedUser = (idUser) => {
+  return axios.delete(`/api/user/bannedUser/${idUser}`);
+};
 
 const createUser = (formData) => {
-    const config = {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      }
-    };
-  
-    return axios.post("/api/user/addNew", formData, config);
+    return axios.post("/api/user/addNew", formData);
+};
+
+const apiLogin = (username, password) => {
+  return axios({
+    url: `/api/user/login?username=${username}&password=${password}`,
+    method: "post",
+  });
+};
+
+const getCartUser = (id) => {
+  return axios({
+    url: `/api/cart/getlistitem?idUser=${id}`,
+    method: "get",
+  });
+};
+
+const deleteCartUser = (idUser, idProduct) => {
+  return axios({
+    url: `/api/cart/removeitems?idUser=${idUser}&items=${idProduct}`,
+    method: "delete",
+  });
+};
+
+const apiCheckOut = (idUser, data) => {
+  return axios({
+    url: `/api/checkout/getinfouser?idUser=${idUser}`,
+    method: "post",
+    data,
+  });
   };
 
 export {
-    getAll  as getAllUsers,
-    createUser 
-}
+    getAllUsers,
+    createUser,
+  apiLogin,
+  deleteCartUser,
+  getCartUser,
+  apiCheckOut,
+  updateUser,
+  bannedUser,
+  getListUsersNotBanned,
+  demo
+};
